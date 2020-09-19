a00c496b3b1d05023d7cbb07a79185c1c8b74360
JeonghwanLee <ljh931111@gmail.com>
2020-09-19 18:24:24 +0900

    2636 solved

--- baekjoon/2636.py
+++ baekjoon/2636.py
@@ -4,27 +4,51 @@ def print_graph(graph):
     print()
 
 n, m = map(int,input().split())
-board = []
 graph = []
 for _ in range(n):
-    board.append(input().split())
+    graph.append(input().split())
 
-stack = [(0,0)]
 visited = []
 for _ in range(n):
     tmp = []
     for _ in range(m):
         tmp.append(False)
     visited.append(tmp)
+visited[0][0] = True
+del_list = []
+depth = -1
+while True:
+    visited_copy = visited
+    for i in range(n):
+        for j in range(m):
+            visited_copy[i][j] = False
 
-while stack:
-    x,y = stack.pop()
+    depth+=1
+    stack = [(0, 0)]
+    while stack:
+        y,x = stack.pop()
+        for delta in (0,-1),(0,1),(1,0),(-1,0):
+            new_y = y+delta[0]
+            new_x = x+delta[1]
+            if not 0<=new_y<n or not 0<=new_x<m:
+                continue
 
+            if graph[new_y][new_x] == '1':
+                if not visited_copy[new_y][new_x]:
+                    visited_copy[new_y][new_x] = True
+                    del_list.append((new_y, new_x))
 
+            else:
+                if not visited_copy[new_y][new_x]:
+                    visited_copy[new_y][new_x] = True
+                    stack.append((new_y, new_x))
 
-    
+    if len(del_list) == 0:
+        break
+    prev = len(del_list)
+    while del_list:
+        dely,delx = del_list.pop()
+        graph[dely][delx] = '0'
 
-
-
-print(max)
-print(answer)
+print(depth)
+print(prev)
