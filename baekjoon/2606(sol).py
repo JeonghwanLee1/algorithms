60e29c3c4a38e4a868b0e090b38087a0aa32cbea
JeonghwanLee1 <ljh931111@gmail.com>
2021-05-25 09:53:08 +0900

    Create 2606(sol).py

---
+++ baekjoon/2606(sol).py
@@ -0,0 +1,19 @@
+N = int(input())
+M = int(input())
+graph = [[] for _ in range(N)]
+for _ in range(M):
+	fr,to = list(map(int,input().split()))
+	graph[fr-1].append(to-1)
+	graph[to-1].append(fr-1)
+
+queue = [0]
+visited = [0]
+while queue:
+	now = queue.pop()
+	adj = graph[now]
+	for elem in adj:
+		if elem not in visited:
+			queue.append(elem)
+			visited.append(elem)
+
+print(len(visited)-1)
